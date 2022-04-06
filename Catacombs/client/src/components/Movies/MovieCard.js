16655836abbe2937a3d7a8a146119a832c726e9a
cbgriffith <c.b.griffith@outlook.com>
2022-04-06 15:55:13 -0400

    final save for the day

--- Catacombs/client/src/components/Movies/MovieCard.js
+++ Catacombs/client/src/components/Movies/MovieCard.js
@@ -11,7 +11,7 @@ export const MovieCard = ({ movie }) => {
             <p>{movie.overview}</p>
             <p>Release Date: {formattedDate}</p>
             <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.original_title}/>
-            <p>{movie.vote_average}</p>
+            <p>User Rating: {movie.vote_average}</p>
             <p>{movie.popularity}</p>
             <hr/>
         </div>
